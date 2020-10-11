eb1364212770a2c2af45c95fc607d0eb45d46144
faithkatherine <faithcathy12@gmail.com>
2020-10-12 00:05:07 +0300

    This is Faith

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizPageComponent } from './quiz-page/quiz-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { WelcomestudentPageComponent } from './welcomestudent-page/welcomestudent-page.component';
+import { StudentcontentPageComponent } from './studentcontent-page/studentcontent-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizPageComponent,
+    SearchPageComponent,
+    WelcomestudentPageComponent,
+    StudentcontentPageComponent
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/studentcontent-page/studentcontent-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studentcontent-page',
+  templateUrl: './studentcontent-page.component.html',
+  styleUrls: ['./studentcontent-page.component.css']
+})
+export class StudentcontentPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
